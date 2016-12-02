5e1f090f8d43410f9378fde4175229a3ea85286c
Mahamudul Haque Medul <mahmudulmedul@gmail.com>
2016-12-03 00:23:03 +0600

    Add files via upload

---
+++ NumberDivisible.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numberdivisible;
+
+/**
+ *
+ * @author Medul
+ */
+public class NumberDivisible {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int i;
+        int count = 0;
+        for(i = 100; i <=1000; i++){
+            if(count <10)
+                if( i % 5 == 0 && i % 6 == 0){
+                    System.out.println(i + "");
+                    count++;
+                }
+                
+            if(count == 10){
+                System.out.println();
+                count = 0;
+            }
+        }
+    }
+    
+}
