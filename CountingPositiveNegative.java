2a39d05c688cc494f9a8a3ac06a47b32d3b93f73
Mahamudul Haque Medul <mahmudulmedul@gmail.com>
2016-12-02 23:22:04 +0600

    Add files via upload

---
+++ CountingPositiveNegative.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package countingpositive.negative;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Medul
+ */
+public class CountingPositiveNegative {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int p=0;
+        int n=0;
+        int data;
+        double total=0;
+        Scanner input= new Scanner(System.in);
+        do{
+            System.out.println("Enter an int value, the program exits if the input is 0: ");
+            data=input.nextInt();
+            total+=data;
+            if(data>0){
+                p++;
+            }
+            if(data<0){
+                n++;
+            }
+                
+                
+        }while(data!=0);
+        double a=(total/(p+n));
+        System.out.println("The number of positives is: "+p);
+        System.out.println("The number of negatives is: "+n);
+        System.out.println("The total is: "+total);
+        System.out.println("The average is: "+a);
+        
+
+    }
+
+}
